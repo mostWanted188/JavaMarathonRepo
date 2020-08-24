f4f5a4fdd257edddcbba71cdb34772f9ffd07d36
Arslan Akhmetzyanov <Arslan.akhmetzyanov@gmail.com>
2020-08-24 20:25:05 +0300

    day1 tasks done

---
+++ src/day1/Task5.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Task5 {
+    public static void main(String[] args){
+        for(int age = 34; age <= 100; age = age + 5){
+            System.out.println("Мой возраст " + age);
+        }
+    }
+}
---
+++ src/day1/Task6.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args){
+        int k = 9;
+        for(int i = 1; i <= 9; i++){
+            int result = i*k;
+            System.out.println(i + " x " + k + " = " + result);
+        }
+    }
+}
